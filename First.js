83419d2ba2f91bc06309170be8521d5e3faaa744
Akansha-Manjrekar <33749763+Akansha-Manjrekar@users.noreply.github.com>
2018-02-27 11:56:43 +0530

    Delete First.js

--- First.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var text = require('./Sample.js');    //If sample.js file present in sam path
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(text.sampleText());      // calls sample text function in Sample.js file
-    res.end();
-
-}).listen(8000);
